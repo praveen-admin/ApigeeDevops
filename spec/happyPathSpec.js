caa1d6dc8ee366f291ba558f77a41498c1888dac
praveen <praveen.opensource@gmail.com>
2016-12-14 19:16:56 +1100

    made changes in variable name

--- spec/happyPathSpec.js
+++ spec/happyPathSpec.js
@@ -4,14 +4,13 @@
 
 var request = require('request');
 var url="http://localhost:9001/";
-var statusCode;
+var statCode;
 describe("To validate API",function () {
    it("it should 200 OK http status code",function (done) {
        request(url,function (err,response,body) {
-           statusCode=response.statusCode;
-           expect(statusCode).toBe(200);
+           statCode=response.statusCode;
+           expect(statCode).toBe(200);
            done();
        });
-      
    }) ;
 });
